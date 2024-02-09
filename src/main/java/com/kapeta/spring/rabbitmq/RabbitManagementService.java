1cd34d183adb89bf20e71953c78041e731db8ae4
Henrik Hofmeister <henrik@newdawn.dk>
2024-02-09 23:51:58 +0100

    fix: Check vhosts using a non-admin method before attempting to create (#5)

--- src/main/java/com/kapeta/spring/rabbitmq/RabbitManagementService.java
+++ src/main/java/com/kapeta/spring/rabbitmq/RabbitManagementService.java
@@ -4,11 +4,16 @@
  */
 package com.kapeta.spring.rabbitmq;
 
+import com.kapeta.spring.config.providers.types.DefaultCredentials;
+import com.kapeta.spring.config.providers.types.InstanceOperator;
+import com.kapeta.spring.rabbitmq.types.RabbitOperatorOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URLEncoder;
@@ -27,36 +32,72 @@ public class RabbitManagementService {
     private final RestTemplate restTemplate = new RestTemplate();
 
     public void ensureVHost(RabbitConnection connection) {
-        int port = 15672;
+
         var operator = connection.getOperator();
         var vhost = connection.getVhost();
 
-        if (operator.getPorts().containsKey(PORT_API)) {
-            port = operator.getPorts().get(PORT_API).getPort();
+        var rabbitMQServer = getBaseUrl(operator);
+        var entity = createEntity(operator);
+
+        String getQueuesUrl = "%s/queues/%s".formatted(rabbitMQServer, URLEncoder.encode(vhost, StandardCharsets.UTF_8));
+        String createVhostsUrl = "%s/vhosts/%s".formatted(rabbitMQServer, URLEncoder.encode(vhost, StandardCharsets.UTF_8));
+
+        log.info("Checking RabbitMQ vhost: {} @ {}", vhost, rabbitMQServer);
+
+        // We ask for queues on the vhost since that does not require any special permissions.
+        // and will return 404 if the vhost does not exist.
+        // It will return 401 if the vhost exist, but we do not have access.
+        try {
+            restTemplate.exchange(getQueuesUrl, HttpMethod.GET, entity, String.class);
+            log.info("RabbitMQ vhost: {} @ {} was found", vhost, rabbitMQServer);
+            return;
+        } catch (RestClientResponseException e) {
+            if (e.getStatusCode().value() != 404) {
+                // If we get here it likely means we do not have access to the vhost
+                // or we do not have access to the management API
+                throw new IllegalStateException("Failed to get vhost: %s @ %s - HTTP status: %s".formatted(vhost, rabbitMQServer, e.getStatusCode().value()), e);
+            }
         }
-        String rabbitMQServer = "http://" + operator.getHostname() + ":" + port;
+
+
+        var wasCreated = defaultRetryTemplate().execute(context -> {
+            try {
+                restTemplate.exchange(createVhostsUrl, HttpMethod.PUT, entity, String.class);
+                return true;
+            } catch (RestClientResponseException e) {
+                if (e.getStatusCode().value() != 499) {
+                    // If we get here it likely means we do not have access to the vhost
+                    // or we do not have access to the management API
+                    throw new IllegalStateException("Failed to create vhost: %s @ %s - HTTP status: %s".formatted(vhost, rabbitMQServer, e.getStatusCode()));
+                }
+                log.error("Failed to create vhost: {} @ {}", vhost, rabbitMQServer, e);
+            }
+            return false;
+        });
+
+        if (!wasCreated) {
+            throw new IllegalStateException("Failed to create vhost: %s".formatted(vhost));
+        }
+
+        log.info("RabbitMQ vhost: {} @ {} was created", vhost, rabbitMQServer);
+
+    }
+
+    private HttpEntity<Void> createEntity(InstanceOperator<RabbitOperatorOptions, DefaultCredentials> operator) {
         String username = operator.getCredentials().getUsername();
         String password = operator.getCredentials().getPassword();
-
-        String url = rabbitMQServer + "/api/vhosts/" + URLEncoder.encode(vhost, StandardCharsets.UTF_8);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setBasicAuth(username, password);
         headers.add("Content-Type", "application/json");
+        return new HttpEntity<>(headers);
+    }
 
-        var entity = new HttpEntity<>(headers);
-
-        log.info("Ensuring RabbitMQ vhost: {} @ {}:{}", vhost, operator.getHostname(), port);
-
-        defaultRetryTemplate().execute(context -> {
-            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.PUT, entity, String.class);
-            if (!response.getStatusCode().is2xxSuccessful()) {
-                throw new RuntimeException("Failed to create vhost: %s - HTTP status: %s".formatted(vhost, response.getStatusCode()));
-            }
-            return null;
-        });
-
-        log.info("RabbitMQ vhost: {} @ {}:{} is ensured", vhost, operator.getHostname(), port);
-
+    private String getBaseUrl(InstanceOperator<RabbitOperatorOptions, DefaultCredentials> operator) {
+        int port = 15672;
+        if (operator.getPorts().containsKey(PORT_API)) {
+            port = operator.getPorts().get(PORT_API).getPort();
+        }
+        return "http://%s:%d/api".formatted(operator.getHostname(), port);
     }
 }
